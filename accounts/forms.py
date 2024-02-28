5b1522b9f441087163bf8ce23b174063495a92b2
krishna <krishnakichu96.kk@gmail.com>
2024-02-28 14:48:29 +0100

    vendor profile form+validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .models import User
-
+from .models import User,UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -22,4 +22,20 @@ class UserForm(forms.ModelForm):
             )
         return self.cleaned_data
 
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing..','required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo= forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields=['profile_picture','cover_photo','address','country','state','city','pin_code','longitude']
+
+   # def _init_(self,*args,**kwargs):
+   #     super(UserProfileForm,self)._init_(*args,**kwargs)
+   #     for field in self.fields:
+   #         if field == 'longitude':
+    #            self.fields[field].widget.attrs['readonly'] ='readonly'
 
